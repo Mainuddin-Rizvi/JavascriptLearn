b5291b11d23ddf5cf1bcb053345e2188e1801e5d
Mainuddin <web97development@gmail.com>
2023-05-25 21:20:46 +0530

    variables let var const

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log(100);
+var eng =85;// funtion scope 
+
+let math = 60; //locally scope
+const pie =3.14; // cant reassign
+
+
+function example() {
+    var x = 10;
+    let y = 20;
+  
+    if (true) {
+      var innerVar = 30;
+      let innerLet = 40;
+      console.log(x);        // 10 (accessible)
+      console.log(y);        // 20 (accessible)
+      console.log(innerVar); // 30 (accessible)
+      console.log(innerLet); // 40 (accessible)
+    }
+  
+    console.log(x);          // 10 (accessible)
+    console.log(y);          // 20 (accessible)
+    console.log(innerVar);   // 30 (accessible)
+    console.log(innerLet);   // ReferenceError: innerLet is not defined
+  }
