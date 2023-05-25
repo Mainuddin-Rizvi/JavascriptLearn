fdc57224d09108839f16ec32c03986abd6ad2621
Mainuddin <web97development@gmail.com>
2023-05-25 21:25:58 +0530

    boolean truthy and falsy value

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
-// String variable declaration
-var message = "Hello, world!";
-let name = "Alice";
-const companyName = "Example Inc.";
+var value1 = "Hello";        // truthy value (non-empty string)
+var value2 = 0;              // falsy value (number zero)
+var value3 = null;           // falsy value (null)
+var value4;                  // falsy value (undefined)
+var value5 = "";             // falsy value (empty string)
+var value6 = "false";        // truthy value (non-empty string)
+var value7 = true;           // truthy value (boolean true)
+var value8 = [];             // truthy value (empty array)
+var value9 = {};             // truthy value (empty object)
+var value10 = function() {}; // truthy value (empty function)
 
-// Template literal usage
-let greeting = `Hello, ${name}!`;
-
-// String method example
-let substringExample = message.substring(7);
-
-// Outputting the results
-console.log(message);           // Output: Hello, world!
-console.log(name);              // Output: Alice
-console.log(companyName);       // Output: Example Inc.
-console.log(greeting);          // Output: Hello, Alice!
-console.log(substringExample);  // Output: world!
+console.log(Boolean(value1)); // Output: true
+console.log(Boolean(value2)); // Output: false
+console.log(Boolean(value3)); // Output: false
+console.log(Boolean(value4)); // Output: false
+console.log(Boolean(value5)); // Output: false
+console.log(Boolean(value6)); // Output: true
+console.log(Boolean(value7)); // Output: true
+console.log(Boolean(value8)); // Output: true
+console.log(Boolean(value9)); // Output: true
+console.log(Boolean(value10)); // Output: true
